d1c5618d9a7daadbcb04599323f6f3163b313652
basementstephen <stephenw.kane@gmail.com>
2013-12-08 16:12:53 -0500

    new treebok version with ruby 1.9.3 and rails 3.2.6 to keep things under control, just added statuses, no bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook5::Application.config.secret_token = '22dc355c5cb0647add9207a533c5c62c23a9a53c03712b025401ea5233d16fc32b6ebfb7dd4b7041b6b3496174cf0c8398912cde1c2d57307dd91551c124b7f1'
